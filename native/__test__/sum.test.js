7aee71dbc761256c6e4fb73a5085bb9d4d9d9278
yayanfr20 <yayanfathurohman20@gmail.com>
2023-09-12 06:47:11 +0700

    unit testing native with node js assert

--- native/__test__/sum.test.js
+++ native/__test__/sum.test.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const sum = require("./sum.js");
+const sum = require("../sum.js");
 
 assert.equal(sum(1, 2), 3);
 assert.throws(() => sum(1, "2"), new Error("invalid arguments"));
